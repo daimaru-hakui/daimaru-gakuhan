d37dfda3dba33b0a2ac7501b4980d67db98c3c7e
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-09-29 11:57:10 +0900

    学生入力値設定

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -8,11 +8,11 @@ import {
   Select,
   Stack,
   Text,
-} from '@chakra-ui/react';
-import { doc, getDoc, Timestamp } from 'firebase/firestore';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import { db } from '../../firebase';
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { collection, doc, getDoc, Timestamp } from "firebase/firestore";
+import { db } from "../../firebase";
 
 type ProjectType = {
   id: string;
@@ -26,13 +26,14 @@ type ProjectType = {
 const Measure = () => {
   const router = useRouter();
   const [project, setProject] = useState<ProjectType>();
+  const [items, setItems] = useState<any>({});
 
+  // project（個別）を取得
   useEffect(() => {
     const getProject = async () => {
-      const docRef = doc(db, 'projects', `${router.query.id}`);
+      const docRef = doc(db, "projects", `${router.query.id}`);
       try {
         const docSnap = await getDoc(docRef);
-        console.log(docSnap.data()?.productName);
 
         if (docSnap.exists()) {
           setProject(docSnap?.data() as ProjectType);
@@ -44,55 +45,119 @@ const Measure = () => {
     getProject();
   }, [router.query.id]);
 
+  const addStudent = () => {
+    const docRef = collection(db, "students");
+    console.log(items);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setItems({ ...items, [name]: value });
+  };
+
+  const handleRadioChange = (e: string) => {
+    const value = e;
+    setItems({ ...items, gender: value });
+  };
+
+  // selectの値を追加・変更
+  const handleSelectChange = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+    rowIndex: number
+  ) => {
+    console.log(e.target.name);
+    const name = e.target.name;
+    setItems(() => {
+      let newItems = [];
+      if (items.products?.length !== rowIndex + 1) {
+        // 値がない場合　追加
+        newItems = [...(items.products || ""), { [name]: e.target.value }];
+      } else {
+        // 値がある場合　更新
+        newItems = items.products?.map((product: any, index: number) => {
+          if (index == rowIndex) {
+            return { ...product, [name]: e.target.value };
+          } else {
+            return product;
+          }
+        });
+      }
+      return { ...items, products: [...(newItems || "")] };
+    });
+  };
+
   return (
-    <Container maxW='600px' py={6}>
+    <Container maxW="600px" py={6}>
       {project?.title && (
         <Box
           p={6}
-          fontSize='3xl'
-          fontWeight='bold'
-          bgColor='white'
+          fontSize="3xl"
+          fontWeight="bold"
+          bgColor="white"
           borderRadius={6}
-          boxShadow='base'
+          boxShadow="base"
         >
           {project?.title}
         </Box>
       )}
-      <Box mt={6} p={6} bgColor='white' borderRadius={6} boxShadow='base'>
+      <Box mt={6} p={6} bgColor="white" borderRadius={6} boxShadow="base">
         <Text>学籍番号</Text>
-        <Input type='text' mt={2} />
+        <Input
+          type="text"
+          mt={2}
+          name="studentNumber"
+          value={items.studentNumber}
+          onChange={handleInputChange}
+        />
       </Box>
-      <Box mt={6} p={6} bgColor='white' borderRadius={6} boxShadow='base'>
+
+      <Box mt={6} p={6} bgColor="white" borderRadius={6} boxShadow="base">
         <Text>名前</Text>
-        <Input type='text' mt={2} />
+        <Input
+          type="text"
+          mt={2}
+          name="name"
+          value={items.name}
+          onChange={handleInputChange}
+        />
       </Box>
-      <Box mt={6} p={6} bgColor='white' borderRadius={6} boxShadow='base'>
-        <RadioGroup defaultValue='3'>
-          <Stack spacing={5} direction='row'>
-            <Radio colorScheme='green' value='1'>
+
+      <Box mt={6} p={6} bgColor="white" borderRadius={6} boxShadow="base">
+        <RadioGroup
+          defaultValue="3"
+          name="gender"
+          onChange={(e) => handleRadioChange(e)}
+        >
+          <Stack spacing={5} direction="row">
+            <Radio colorScheme="green" value="1">
               男性
             </Radio>
-            <Radio colorScheme='green' value='2'>
+            <Radio colorScheme="green" value="2">
               女性
             </Radio>
-            <Radio colorScheme='green' value='3'>
+            <Radio colorScheme="green" value="3">
               未記入
             </Radio>
           </Stack>
         </RadioGroup>
       </Box>
-      {project?.products.map((product: any) => (
+      {project?.products.map((product: any, index: number) => (
         <Box
           key={product.id}
           mt={6}
           p={6}
-          bgColor='white'
+          bgColor="white"
           borderRadius={6}
-          boxShadow='base'
+          boxShadow="base"
         >
           <Box>{product.productName}</Box>
           <Box mt={6}>
-            <Select placeholder='サイズを選択してください'>
+            <Select
+              placeholder="サイズを選択してください"
+              name="size"
+              onChange={(e) => handleSelectChange(e, index)}
+            >
               {product.size.map((size: string) => (
                 <option key={size} value={size}>
                   {size}
@@ -100,10 +165,14 @@ const Measure = () => {
               ))}
             </Select>
             <Box mt={6}>
-              {product.type === '1' && (
-                <Select placeholder='数量を選択してしてください'>
+              {product.type === "1" && (
+                <Select
+                  name="quantity"
+                  placeholder="数量を選択してしてください"
+                  onChange={(e) => handleSelectChange(e, index)}
+                >
                   {[...Array(10)].map((num: string, index: number) => (
-                    <option key={num} value={index}>
+                    <option key={num?.toString()} value={index}>
                       {index}
                     </option>
                   ))}
@@ -113,8 +182,10 @@ const Measure = () => {
           </Box>
         </Box>
       ))}
-      <Box mt={6} textAlign='center'>
-        <Button colorScheme='facebook'>登録</Button>
+      <Box mt={6} textAlign="center">
+        <Button colorScheme="facebook" onClick={addStudent}>
+          登録
+        </Button>
       </Box>
     </Container>
   );
